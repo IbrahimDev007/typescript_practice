a16a73fd9dc9ab2367b02e8cc4db03e6d66134a0
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-10-19 11:24:58 +0600

    basic types and typscrip initialize to compile

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+// Basic Types
+let id = 5;
+let company = 'Ibrahim khalil ullah';
+let isPublished = true;
+let x = 'Hello';
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'Hello'];
+console.log(id, arr, typeof arr);
---
+++ index.ts
@@ -0,0 +1,11 @@
+
+// Basic Types
+let id: number = 5
+let company: string = 'Ibrahim khalil ullah'
+let isPublished: boolean = true
+let x: any = 'Hello'
+
+let ids: number[] = [1, 2, 3, 4, 5]
+let arr: any[] = [1, true, 'Hello']
+
+console.log(id,arr, typeof arr);
