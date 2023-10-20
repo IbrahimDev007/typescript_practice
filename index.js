9598c325ef805cd5e8fab06b8cda49027532460a
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-10-20 22:18:50 +0600

    typscript tupple array

--- index.js
+++ index.js
@@ -6,4 +6,13 @@ let isPublished = true;
 let x = 'Hello';
 let ids = [1, 2, 3, 4, 5];
 let arr = [1, true, 'Hello'];
-console.log(id, arr, typeof arr);
+// Tuple
+let person = [1, 'Brad', true];
+// Tuple Array
+let employees;
+employees = [
+    [1, 'Brad'],
+    [2, 'John'],
+    [3, 'Jill'],
+];
+console.log(employees);
--- index.ts
+++ index.ts
@@ -8,4 +8,14 @@ let x: any = 'Hello'
 let ids: number[] = [1, 2, 3, 4, 5]
 let arr: any[] = [1, true, 'Hello']
 
-console.log(id,arr, typeof arr);
+// Tuple
+let person: [number, string, boolean] = [1, 'Brad', true]
+// Tuple Array
+let employees: [number, string][]
+
+employees = [
+  [1, 'Brad'],
+  [2, 'John'],
+  [3, 'Jill'],
+]
+console.log(employees);
