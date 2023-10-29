651ab4e595396fdfb8440ecf57dd6cb2f623cada
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-10-29 19:57:32 +0600

    class added

--- index.js
+++ index.js
@@ -1,18 +1,76 @@
-"use strict";
-// Basic Types
-let id = 5;
-let company = 'Ibrahim khalil ullah';
-let isPublished = true;
-let x = 'Hello';
-let ids = [1, 2, 3, 4, 5];
-let arr = [1, true, 'Hello'];
-// Tuple
-let person = [1, 'Brad', true];
-// Tuple Array
-let employees;
-employees = [
-    [1, 'Brad'],
-    [2, 'John'],
-    [3, 'Jill'],
+// basic types
+var i = 4;
+var j = "ibrahim";
+var k = true;
+var m = "true";
+var n = [1, 2, 3, 4, 5, 6, 7];
+//tuple of typescript
+var even = [
+    2,
+    4,
+    6,
+    8,
+    10,
+    12,
+    "string",
+    14,
 ];
-console.log(employees);
+var odd;
+odd = [
+    ["1st ", 1],
+    ["2nd ", 2],
+];
+//typescript union  veritable
+var union = "99";
+//enum in type script
+var Direction;
+(function (Direction) {
+    Direction[Direction["up"] = 1] = "up";
+    Direction[Direction["down"] = 2] = "down";
+    Direction[Direction["right"] = 3] = "right";
+    Direction[Direction["left"] = 4] = "left";
+})(Direction || (Direction = {}));
+console.log(Direction);
+var obj = {
+    id: 1334,
+    name: "jhon",
+};
+//  assertation   ,changing type
+var cid = "dude";
+var customerId = cid;
+//functon
+function addNum(x, y) {
+    return x + y;
+}
+//no return type
+function msg() {
+    console.log("this is typscript function");
+}
+//two declaration typscript type,interface
+console.log(typeof "peacook", "peacook");
+var MathExam = {
+    cid: 133,
+    acid: 235,
+    name: "jony",
+    msg: function () { return console.log("hellow math student"); },
+};
+var Mathematics = /** @class */ (function () {
+    function Mathematics() {
+        this.cid = 134;
+        this.acid = 2353;
+        this.name = "random";
+    }
+    Mathematics.prototype.msg = function () {
+        console.log("Hello math student");
+    };
+    Mathematics.prototype.randomSum = function () {
+        return this.cid + this.acid;
+    };
+    return Mathematics;
+}());
+var mathObject = new Mathematics();
+console.log(mathObject.cid); // Output: 134
+console.log(mathObject.name); // Output: "random"
+mathObject.msg(); // Output: "Hello math student"
+mathObject.id = 10; // Assign a value to the id property
+console.log(mathObject.randomSum()); // Output: 2363 (134 + 2353)
--- index.ts
+++ index.ts
@@ -1,21 +1,117 @@
+// basic types
+let i: number = 4;
+let j: string = "ibrahim";
+let k: boolean = true;
+let m: any = "true";
+let n: number[] = [1, 2, 3, 4, 5, 6, 7];
+//tuple of typescript
 
-// Basic Types
-let id: number = 5
-let company: string = 'Ibrahim khalil ullah'
-let isPublished: boolean = true
-let x: any = 'Hello'
+let even: [number, number, number, number, number, number, string, number] = [
+	2,
+	4,
+	6,
+	8,
+	10,
+	12,
+	"string",
+	14,
+];
+let odd: [string, number][];
+odd = [
+	["1st ", 1],
+	["2nd ", 2],
+];
 
-let ids: number[] = [1, 2, 3, 4, 5]
-let arr: any[] = [1, true, 'Hello']
+//typescript union  veritable
 
-// Tuple
-let person: [number, string, boolean] = [1, 'Brad', true]
-// Tuple Array
-let employees: [number, string][]
+let union: string | number = "99";
 
-employees = [
-  [1, 'Brad'],
-  [2, 'John'],
-  [3, 'Jill'],
-]
-console.log(employees);
+//enum in type script
+
+enum Direction {
+	up = 1,
+	down,
+	right,
+	left,
+}
+
+console.log(Direction);
+
+//object
+type obj = {
+	id: number;
+	name: string;
+};
+
+let obj: obj = {
+	id: 1334,
+	name: "jhon",
+};
+
+//  assertation   ,changing type
+
+let cid: any = "dude";
+let customerId = <number>cid;
+
+//functon
+function addNum(x: number, y: number): number {
+	return x + y;
+}
+//no return type
+function msg(): void {
+	console.log("this is typscript function");
+}
+//two declaration typscript type,interface
+
+console.log(typeof "peacook", "peacook");
+
+//interfaces  typscript
+
+interface objectExam {
+	readonly cid: number; //seems like private method
+	acid: number;
+	name?: string;
+	msg: () => void;
+}
+
+let MathExam: objectExam = {
+	cid: 133,
+	acid: 235,
+	name: "jony",
+	msg: () => console.log("hellow math student"),
+};
+
+type objectExams = {
+  cid: number;
+  name?: string;
+  msg: () => void;
+};
+
+class Mathematics implements objectExams {
+  readonly cid: number;
+  acid: number;
+  name: string;
+  id!: number; // Definite assignment assertion for id
+
+  constructor() {
+      this.cid = 134;
+      this.acid = 2353;
+      this.name = "random";
+  }
+
+  msg() {
+      console.log("Hello math student");
+  }
+
+  randomSum(): number {
+      return this.cid + this.acid;
+  }
+}
+
+const mathObject = new Mathematics();
+console.log(mathObject.cid); // Output: 134
+console.log(mathObject.name); // Output: "random"
+mathObject.msg(); // Output: "Hello math student"
+
+mathObject.id = 10; // Assign a value to the id property
+console.log(mathObject.randomSum()); // Output: 2363 (134 + 2353)
